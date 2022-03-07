04051230f858339958f1e37069f52a47755f3ce3
Ruler90 <54283970+Ruler90@users.noreply.github.com>
2022-03-07 23:13:16 +0100

    Bookmarklets - updated OKO.press dark theme code

--- js-bookmarklets/okoPressDarkTheme.js
+++ js-bookmarklets/okoPressDarkTheme.js
@@ -4,13 +4,20 @@ const darkThemeHandler = () => {
         .off-canvas-content, #banner-after-excerpt, section, ul, .page-like .like-links-block, .tabs-content {
             background-color: #123 !important;
         }
+        
         h1, h2, h3, h4, h5, h6, p, span, li, article, a:not(.wesprzyj):not(.ScrollToComment) {
             background: none !important;
             color: #fff !important;
         }
+        
+        #nav-icon2 span {
+            background-color: #9e1c1c !important;
+        }
+        
         iframe {
             background-color: #fff;
         }
+        
         section.break-1 .after-break .content-wrapper {
             background: none;
         }
